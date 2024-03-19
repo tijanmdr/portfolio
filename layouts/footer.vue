f62f934f680c852e52f7ae3788dd8a5e76fad3b8
tijan <tijanmdR@gmail.com>
2024-03-20 08:06:49 +1100

    fix

---
+++ layouts/footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <nuxt-link to="/">Home</nuxt-link>
+  <nuxt-link to="/blog">Blogs</nuxt-link>
+  <nuxt-link to="/contact">Contact</nuxt-link>
+</template>
+<script setup lang="ts">
+</script>
