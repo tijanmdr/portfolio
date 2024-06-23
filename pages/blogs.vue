d89ffed8ca302a0b9f78f27529e7484b1be37a4e
tijan <tijanmdR@gmail.com>
2024-06-23 21:13:03 +1000

    fix

--- pages/blogs.vue
+++ pages/blogs.vue
@@ -26,33 +26,11 @@
 import {useRouter} from "vue-router";
 
 useHead({
-  title: 'Blog List | Tijan Manandhar',
+  title: 'Blogs | Tijan Manandhar',
 })
 
 const router = useRouter()
 
 let _posts = []
 _posts = await $fetch('https://dev.to/api/articles?username=tijanmdr').catch((error) => error.data);
-
-console.log(_posts);
-
-const posts = [
-  {
-    id: 1,
-    title: 'Boost your conversion rate',
-    slug: '/boost-your-conversion-rate',
-    description:
-        'Illo sint voluptas. Error voluptates culpa eligendi. Hic vel totam vitae illo. Non aliquid explicabo necessitatibus unde. Sed exercitationem placeat consectetur nulla deserunt vel. Iusto corrupti dicta.',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
-    category: { title: 'Marketing', href: '#' },
-    author: {
-      name: 'Michael Foster',
-      role: 'Co-Founder / CTO',
-      href: '#',
-      imageUrl:
-          'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  }
-]
 </script>
