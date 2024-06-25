40300dce74bd91f2a9dc098aa1aab2dd418fff5e
Tijan Manandhar <tijanmdr@gmail.com>
2024-06-25 16:46:10 +1000

    Update about.vue

--- pages/about.vue
+++ pages/about.vue
@@ -3,19 +3,19 @@
         <div
             class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3"
         ></div>
-        <div class="flex flex-row">
-            <div class="max-md:w-full max-lg:w-1/2 lg:w-2/3 md:px-10 max-lg:order-2 py-10 items-items-center text-center lg:text-2xl lg:leading-10">
-                <p>📸 Hi! I’m Tijan Manandhar, a web developer who dances with code and captures moments through a camera lens.</p>
-                <p>🎨 When I’m not crafting backends with Node.js and PHP, I’m busy painting user interfaces with Vue.js and React.</p>
-                <p>🎵 Music fuels my creativity, and hiking clears my cache—essential for a balanced developer life.</p>
-                <p>🚀 Connect with me to build something awesome together!</p>
-                <p>👩‍💻 A proud GDG Canberra co-organizer.</p>
-            </div>
-            <div class="justify-center max-lg:w-1/2 lg:w-1/3 max-lg:order-1 max-md:w-full px-10">
-                <img class="rounded-full" src="/img/portrait.webp" />
-            </div>
+        <div class="flex max-md:flex-col md:flex-row">
+  <div class="py-10 text-center leading-8 max-lg:order-2 max-lg:w-1/2 max-md:w-full max-md:px-10 md:px-10 lg:w-2/3 lg:text-2xl lg:leading-10">
+    <p>📸 Hi! I’m Tijan Manandhar, a web developer who dances with code and captures moments through a camera lens.</p>
+    <p>🎨 When I’m not crafting backends with Node.js and PHP, I’m busy painting user interfaces with Vue.js and React.</p>
+    <p>🎵 Music fuels my creativity, and hiking clears my cache—essential for a balanced developer life.</p>
+    <p>🚀 Connect with me to build something awesome together!</p>
+    <p>👩‍💻 A proud GDG Canberra co-organizer.</p>
+  </div>
+  <div class="flex justify-center px-10 max-lg:order-1 max-lg:w-1/2 max-md:w-full lg:w-1/3">
+    <img class="rounded-full" src="https://th.bing.com/th/id/OIP.FDbWVuNc7dA-5WAPXW9wZAHaJN?rs=1&pid=ImgDetMain" />
+  </div>
+</div>
 
-        </div>
 
     </div>
 </template>
